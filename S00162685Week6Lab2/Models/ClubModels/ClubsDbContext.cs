5e8b6607b625dee0d017c7d5014799cc6649489c
PsychoHacks <jamie.v732@gmail.com>
2017-11-10 11:59:47 +0000

    Added ClubModelMigration and some test data. Second half of class 10/11/2017

---
+++ S00162685Week6Lab2/Models/ClubModels/ClubsDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Rad2016SampleWepApp.Models.ClubModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace S00162685Week6Lab2.Models.ClubModels
+{
+    public class ClubsDbContext : DbContext
+    {
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<ClubEvent> ClubEvents { get; set; }
+
+        public ClubsDbContext()
+            : base("DefaultConnection")
+        {
+        }
+    }
+}
